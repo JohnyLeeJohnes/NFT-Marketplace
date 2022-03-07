d5b1ba0458d58c04a7b8799480b0010c21bacd91
JohnyLeeJohnes <41521818+JohnyLeeJohnes@users.noreply.github.com>
2022-03-07 16:56:35 +0100

    Frontend - Created title and navbar

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,48 @@
+import React from 'react';
+import Link from 'next/Link'
 import '../styles/globals.css'
+import 'antd/dist/antd.css';
+import {Menu, Space, Typography} from 'antd';
+import {WalletOutlined, HomeOutlined, DashboardOutlined, UnorderedListOutlined} from '@ant-design/icons';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const {Title} = Typography;
+
+function App({Component, pageProps}) {
+    return (
+        <div>
+            <Space direction="horizontal" style={{width: '100%', justifyContent: 'center'}}>
+                <Typography>
+                    <Title>Welcome to Johny NFT Marketplace</Title>
+                </Typography>
+            </Space>
+
+            <Menu mode="horizontal" style={{justifyContent: 'center'}}>
+                <Menu.Item key="home" icon={<HomeOutlined/>}>
+                    <Link href="/home">
+                        <a>Home</a>
+                    </Link>
+                </Menu.Item>
+                <Menu.Item key="dashboard" icon={<DashboardOutlined/>}>
+                    <Link href="/dashboard">
+                        <a>NFT Dashboard</a>
+                    </Link>
+                </Menu.Item>
+                <Menu.Item key="sell" icon={<WalletOutlined/>}>
+                    <Link href="/sell">
+                        <a>Sell NFT</a>
+                    </Link>
+                </Menu.Item>
+                <Menu.Item key="collection" icon={<UnorderedListOutlined/>}>
+                    <Link href="/collection">
+                        <a>My NFTs</a>
+                    </Link>
+                </Menu.Item>
+            </Menu>
+            <Component {...pageProps} />
+
+        </div>
+    )
+        ;
 }
 
-export default MyApp
+export default App
