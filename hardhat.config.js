6cfcd6210aabe09853958c406aa1e78ef1e1612d
JohnyLeeJohnes <41521818+JohnyLeeJohnes@users.noreply.github.com>
2022-03-10 22:13:58 +0100

    Testing MetaMask key

--- hardhat.config.js
+++ hardhat.config.js
@@ -15,11 +15,11 @@ module.exports = {
         },
         mumbai: {
             url: "https://polygon-mumbai.infura.io/v3/44c7220a91f547cb81c8905063bccee1",
-            accounts: ["6489e7c9bf31d82075033e6d95da07a16fef0eea4dbf89f3285dda78453bf1f7"]
+            accounts: []
         },
         mainnet: {
             url: "https://polygon-mainnet.infura.io/v3/44c7220a91f547cb81c8905063bccee1",
-            accounts: ["6489e7c9bf31d82075033e6d95da07a16fef0eea4dbf89f3285dda78453bf1f7"]
+            accounts: []
         }
     },
     solidity: {
