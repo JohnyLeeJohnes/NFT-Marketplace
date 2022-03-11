71acd97ea33d57da59b3840ca58dcc40566c0c0a
JohnyLeeJohnes <41521818+JohnyLeeJohnes@users.noreply.github.com>
2022-03-11 21:31:18 +0100

    Added login account button Changed RPC

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,6 @@
 require("@nomiclabs/hardhat-waffle");
 
-//const infuraId = "44c7220a91f547cb81c8905063bccee1"
+const infuraId = "44c7220a91f547cb81c8905063bccee1"
 
 module.exports = {
     defaultNetwork: "hardhat",
@@ -9,13 +9,13 @@ module.exports = {
             chainId: 1337
         },
         mumbai: {
-            //url: `https://polygon-mumbai.infura.io/v3/${infuraId}`,
-            url: "https://rpc-mumbai.matic.today",
+            url: `https://polygon-mumbai.infura.io/v3/${infuraId}`,
+            //url: "https://rpc-mumbai.matic.today",
             accounts: [process.env.METAMASK_KEY]
         },
         matic: {
-            //url: `https://polygon-mainnet.infura.io/v3/${infuraId}`,
-            url: "https://rpc-mainnet.maticvigil.com",
+            url: `https://polygon-mainnet.infura.io/v3/${infuraId}`,
+            //url: "https://rpc-mainnet.maticvigil.com",
             accounts: [process.env.METAMASK_KEY]
         }
     },
--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,18 +1,35 @@
-import React, {Component} from 'react';
+import React, {Component, useState} from 'react';
+import {ethers} from "ethers";
 import 'antd/dist/antd.css';
-import {Layout} from 'antd';
+import {Button, Layout, Typography} from 'antd';
 import {useTranslation} from "../utils/use-translations";
 import '../styles/index.css';
 import logo from "../public/logo-black.svg"
 import Image from "next/image"
 import {ContractAddressProvider, MenuComponent, MenuSelectionProvider} from "../components";
 import Head from "next/head";
+import Web3Modal from "web3modal";
 
 const {Header, Content, Footer} = Layout;
 
 
 function App({Component, pageProps}) {
+    const [account, setAccount] = useState([])
     const {t} = useTranslation()
+
+    async function getWalletAddress() {
+        try {
+            const w3m = new Web3Modal()
+            const w3mConnection = await w3m.connect()
+            const provider = new ethers.providers.Web3Provider(w3mConnection)
+            await provider.send("eth_requestAccounts", []);
+            const signer = provider.getSigner();
+            const address = await signer.getAddress()
+            setAccount(address)
+        } catch (e) {
+            console.log(e)
+        }
+    }
 
     return (
         <ContractAddressProvider>
@@ -24,6 +41,12 @@ function App({Component, pageProps}) {
                     <Header style={{background: '#fff'}}>
                         <div className={"logo"}>
                             <Image width={150} height={80} src={logo} alt={"logo"}/>
+                        </div>
+                        <div className={"address"}>
+                            <Typography.Title level={3} style={{align: "right"}}>
+                                <Button onClick={getWalletAddress} shape="round"
+                                        danger>{t("Login to MetaMask")}</Button>
+                            </Typography.Title>
                         </div>
                         <MenuComponent/>
                     </Header>
