709ca470d237f13f1e40fe773b3cad57ed119213
JohnyLeeJohnes <41521818+JohnyLeeJohnes@users.noreply.github.com>
2022-03-15 23:05:19 +0100

    Moved to private RPC

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,13 +7,13 @@ module.exports = {
             chainId: 1337
         },
         mumbai: {
-            url: `https://polygon-mumbai.infura.io/v3/44c7220a91f547cb81c8905063bccee1`,
-            //url: "https://rpc-mumbai.matic.today",
+            //url: `https://polygon-mumbai.infura.io/v3/44c7220a91f547cb81c8905063bccee1`,
+            url: "https://rpc-mumbai.matic.today",
             accounts: [process.env.METAMASK_KEY]
         },
         matic: {
-            url: `https://polygon-mainnet.infura.io/v3/44c7220a91f547cb81c8905063bccee1`,
-            //url: "https://rpc-mainnet.matic.network",
+            //url: `https://polygon-mainnet.infura.io/v3/44c7220a91f547cb81c8905063bccee1`,
+            url: "https://rpc-mainnet.matic.network",
             accounts: [process.env.METAMASK_KEY]
         }
     },
